d383671d0a5ca08296162d5d045ab72cacbcd588
zhaichenkai <649844514@qq.com>
2019-08-23 09:50:22 +0800

    hook

--- src/views/PersonalSettings/index.js
+++ src/views/PersonalSettings/index.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import { Card } from 'antd'
+import React, { useReducer } from 'react';
+import { Card, Button } from 'antd'
 
-class PersonalSettings extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (  
-            <Card title='个人设置' bordered={false} >
-
-            </Card>
-        );
-    }
+function Settings(){
+    const ADD = 'ADD' 
+    const SUB = 'SUB'
+    const [ count, dispatch ] = useReducer((state,action)=>{
+        switch(action.type){
+            case ADD:
+                return state+1
+            case SUB:
+                return state-1
+            default:
+                return state
+        }
+    },0)
+    return (  
+        <Card title='个人设置' bordered={false} >
+            <h2>{count}</h2>
+            <Button onClick={()=>dispatch({type:ADD})}>Add</Button>
+            <Button onClick={()=>dispatch({type:SUB})}>Sub</Button>
+        </Card>
+    )
 }
  
-export default PersonalSettings;
+export default Settings;
